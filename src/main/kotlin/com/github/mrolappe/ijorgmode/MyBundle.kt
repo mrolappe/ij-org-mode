56bfbf23f21c601b3e469998da4cba74157951be
GitHub Action <action@github.com>
2024-07-24 10:53:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/mrolappe/ijorgmode/MyBundle.kt
+++ src/main/kotlin/com/github/mrolappe/ijorgmode/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.mrolappe.ijorgmode
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/mrolappe/ijorgmode/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mrolappe/ijorgmode/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.mrolappe.ijorgmode.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mrolappe.ijorgmode.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
